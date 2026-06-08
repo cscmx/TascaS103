e236745fe234d46111cf734259a36c954f48e2ca
cscmx <sainz.carolina@gmail.com>
2026-06-08 11:02:05 +0200

    Explanation of the problem added to Tasca3.ejercicio1

--- ejercicio1.php
+++ ejercicio1.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Crea una función que reciba como parámetros un array de palabras y un carácter. La función nos devuelve true si todas las palabras del array tienen el carácter pasado como segundo parámetro.
+Crea un array, afegeix-li 5 nombres enters i després mostrals per pantalla d’un en un.
 */
 
 $arrayNumbers = [1,2,3,4,5];
